a47abc0d747440553c9007f5581cc23de437050a
Victor Martins <victor.martins.duarte@gmail.com>
2022-05-23 15:23:12 -0300

    Feat: Req-2 Query to count songs, artists and albums

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+SELECT
+  COUNT(DISTINCT so.song_id) AS cancoes,
+  COUNT(DISTINCT ar.artist_id) AS artistas,
+  COUNT(DISTINCT al.album_id) AS albuns
+FROM
+  SpotifyClone.songs AS so,
+  SpotifyClone.artists AS ar,
+  SpotifyClone.albums AS al;
